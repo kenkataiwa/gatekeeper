275d0c2659e45433d9db7681d8655d49be42c1c8
Kenneth Kataiwa <kenkataiwa@gmail.com>
2013-04-08 15:53:46 +0300

    Adding sandbox

---
+++ tools/sandbox/index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sandbox;
+
+spl_autoload_register(function($className) {
+            $className = (string) str_replace('\\', DIRECTORY_SEPARATOR, $className);
+            $className .= '.php';
+            $className = __DIR__ . '/../../lib/' . $className;
+            if (file_exists($className)) {
+                require_once $className;
+            }
+        });
+
+use \OAuth\Auth;
+
+try {
+    $auth = new Auth(require __DIR__ . '/config/services.config.php');
+    $adapter = $auth->authenticate('facebook', array());
+    $userProfile = $adapter->getUserProfile();
+} catch (Exception $e) {
+    die("<b>got an error!</b> " . $e->getMessage());
+}
+
+print_r($userProfile);
