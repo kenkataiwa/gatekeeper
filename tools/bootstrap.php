2dc9eb334444d0fa1bfc410b57849bc5a42fc48a
Kenneth Kataiwa <kenkataiwa@gmail.com>
2013-05-09 14:17:35 +0300

    Bootstraping

--- tools/bootstrap.php
+++ tools/bootstrap.php
@@ -4,12 +4,3 @@
  * Requiring composer dependencies autoloader
  */
 require __DIR__ . '/../vendor/autoload.php';
-
-spl_autoload_register(function($className) {
-            $className = (string) str_replace('\\', DIRECTORY_SEPARATOR, $className);
-            $className .= '.php';
-            $className = __DIR__ . '/../lib/' . $className;
-            if (file_exists($className)) {
-                require_once $className;
-            }
-        });
