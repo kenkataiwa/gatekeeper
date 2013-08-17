336a9feee3ad42b3d0c07f598edcac523a4f6439
Kenneth Kataiwa <kenkataiwa@gmail.com>
2013-08-17 12:10:56 +0300

    Fixing sandbox

--- tools/connect/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require __DIR__.'/../bootstrap.php';
-
-\Gatekeeper\Endpoint::process();
--- tools/sandbox/index.php
+++ tools/sandbox/index.php
@@ -2,11 +2,9 @@
 
 require __DIR__ . '/../bootstrap.php';
 
-
-
 try {
     $auth = new \Gatekeeper\Auth(require __DIR__ . '/config/services.config.php');
-    $google = $auth->authenticate('twitter');
+    $google = $auth->authenticate('facebook');
     $userProfile = $google->getUserProfile();
 } catch (\Exception $e) {
     die("<b>got an error!</b> " . $e->getMessage());
