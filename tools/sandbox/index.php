da93893d325c7ea10d8048ecdee5f56b2200dece
Kenneth Kataiwa <kenkataiwa@gmail.com>
2014-09-25 14:23:25 +0300

    Add favicon, update .gitignore

--- tools/sandbox/index.php
+++ tools/sandbox/index.php
@@ -3,15 +3,14 @@
 require __DIR__ . '/../bootstrap.php';
 
 try {
-    $params = ['business' => 'http://www.marketplace.co.tz/CenturyCinemax'];
     $auth = new \Gatekeeper\Auth(require __DIR__ . '/config/services.config.php');
-    $service = $auth->authenticate('facebook');
-    //$p = $service->publishUserAction('marketplacetz:review', $params);
+    $service = $auth->authenticate('google');
+    $user = $service->getUserProfile();
 } catch (\Exception $e) {
     //var_dump($e);
     die("<b>Got an error!</b> " . $e->getMessage());
 }
 
-var_dump($p);
+var_dump($user);
 
 echo 'Finished execution';
